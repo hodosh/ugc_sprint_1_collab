a13153b478c4b763f63820fb7a0d67e273411bf7
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-15 22:52:53 +0300

    add clickhouse client

--- etl/src/services/clickhouse.py
+++ etl/src/services/clickhouse.py
@@ -1,3 +1,44 @@
 from clickhouse_driver import Client
 
-client = Client(host='localhost')
+from src.utils import backoff
+
+
+class ClickHouseLoader:
+    def __init__(self,
+                 host: str,
+                 database: str,
+                 table: str,
+                 batch_size: int = 500,
+                 cluster: str = 'company_cluster'):
+        self._host = host
+        self._database = database
+        self._table = table
+        self._batch_size = batch_size
+        self._cluster = cluster
+        self._client = None
+
+    @property
+    def client(self):
+        if not self._client:
+            self._client = Client(host=self._host)
+
+        return self._client
+
+    @backoff()
+    def load(self, data: dict):
+        """
+        Метод загружает данные в таблицу
+        :param data: словарь данных
+        """
+        for batch_data in data:
+            self.client.execute(
+                f'INSERT INTO {self._database}.{self._table} VALUES',
+                batch_data,
+            )
+
+    @backoff()
+    def create_table(self):
+        """
+        Создаем таблицу, если она отсутствует
+        """
+        self.client.execute(f'CREATE DATABASE IF NOT EXISTS {self._table} ON CLUSTER {self._cluster}')
