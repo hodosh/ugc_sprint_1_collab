741df3f03eb9117616569484b3abef749dd31aca
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-16 22:08:47 +0300

    fix settings & add env.example

--- etl/src/config.py
+++ etl/src/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     kafka_brokers = Field(env='KAFKA_BROKERS', default='localhost:9092')
     kafka_topics = Field(env='KAFKA_TOPICS', default='events_topic')
     kafka_group_id = Field(env='KAFKA_GROUP_ID', default='group_id_0')
-    clickhouse_host = Field(env='CLICKHOUSE_HOST', default='')
+    clickhouse_host = Field(env='CLICKHOUSE_HOST', default='localhost')
     clickhouse_database = Field(env='CLICKHOUSE_DATABASE', default='events')
     clickhouse_table = Field(env='CLICKHOUSE_TABLE', default='events')
     clickhouse_cluster = Field(env='CLICKHOUSE_CLUSTER', default='company_cluster')
