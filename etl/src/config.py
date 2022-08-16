ab7323213424b7b8726365b2acf9a12cf5e3f7bd
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-16 23:02:51 +0300

    fix kafka configuration

--- etl/src/config.py
+++ etl/src/config.py
@@ -2,10 +2,10 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    kafka_brokers = Field(env='KAFKA_BROKERS', default='localhost:9092')
+    kafka_brokers = Field(env='KAFKA_BROKERS', default='127.0.0.1:9094')
     kafka_topics = Field(env='KAFKA_TOPICS', default='events_topic')
     kafka_group_id = Field(env='KAFKA_GROUP_ID', default='group_id_0')
-    clickhouse_host = Field(env='CLICKHOUSE_HOST', default='localhost')
+    clickhouse_host = Field(env='CLICKHOUSE_HOST', default='127.0.0.1')
     clickhouse_database = Field(env='CLICKHOUSE_DATABASE', default='events')
     clickhouse_table = Field(env='CLICKHOUSE_TABLE', default='events')
     clickhouse_cluster = Field(env='CLICKHOUSE_CLUSTER', default='company_cluster')
