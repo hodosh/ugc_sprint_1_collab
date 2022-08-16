fd0f183940d65410d7dd0cbbbdcb85bb6654aa9e
Ilia Suvorkin <ilyafx@gmail.com>
2022-08-17 02:14:54 +0300

    Batch insert in ClickHouse

--- research/bench/app.py
+++ research/bench/app.py
@@ -22,7 +22,7 @@ class BenchMark:
         self.movie_lengths = [randrange(config.MOVIE_MAX_LEN) for i in range(1, config.USER_COUNT)]
         self.progress = Progress()
 
-    def benchmark_service_once(self, service):
+    def benchmark_service_single(self, service):
         time_now = datetime.now()
         time_now = time_now.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -37,30 +37,53 @@ class BenchMark:
                 )
                 service.insert(data=data)
 
-    def benchmark_service(self, storage):
+    def benchmark_service_batch(self, service):
+        time_now = datetime.now()
+        time_now = time_now.strftime("%Y-%m-%d %H:%M:%S")
+        data = []
+
+        for user_counter in range(1, config.USER_COUNT - 1):
+            user_current_movie_id = self.user_movie[user_counter]
+            for tick in range(1, self.movie_lengths[user_counter]):
+                row = MovieViewEvent(
+                    movie_id=str(user_current_movie_id),
+                    user_id=str(self.users[user_counter]),
+                    event_time=time_now,
+                    view_run_time=tick
+                )
+                data.append(row)
+
+        service.insert_batch(data=data)
+
+    def benchmark_service(self, storage, mode):
         self.tasks[storage] = self.progress.add_task(f"[cyan]{storage}", total=config.BATCHES - 1)
         service = BATCH_SEQUENCE[storage]['client']
         for i in range(1, config.BATCHES):
-            self.benchmark_service_once(service=service)
+            if mode == "single":
+                self.benchmark_service_single(service=service)
+            if mode == "batch":
+                self.benchmark_service_batch(service=service)
+
             self.progress.update(self.tasks[storage], advance=1)
 
     def run(self):
         statistics = []
         with Progress() as progress:
             self.progress = progress
-            for storage in BATCH_SEQUENCE:
-                if BATCH_SEQUENCE[storage]['use']:
+            for item in BATCH_SEQUENCE:
+                if BATCH_SEQUENCE[item]['use']:
+                    mode = BATCH_SEQUENCE[item]['mode']
+                    storage_name = BATCH_SEQUENCE[item]['storage']
                     t0 = time.time()
 
-                    self.benchmark_service(storage)
+                    self.benchmark_service(item, mode)
 
                     t1 = time.time() - t0
-                    row_stat = {'storage': storage, 'mode': 'single', 'runtime': t1}
+                    row_stat = {'storage': storage_name, 'mode': mode, 'runtime': t1}
                     statistics.append(row_stat)
         return statistics
 
     def show_result(self, statistics):
-        global app
         table = Table(title="OLAP Storage Research")
 
         table.add_column("Storage", style="magenta")
--- research/bench/benchmarks.py
+++ research/bench/benchmarks.py
@@ -1,14 +1,31 @@
 import config
 from hl_storage_kafka import KafkaStorage
 from hl_storage_clickhouse import ClickhouseStorage
+from hl_storage_null import DevNullStorage
 
 BATCH_SEQUENCE = {
     'Kafka': {
+        'storage': 'Kafka',
         'client': KafkaStorage(connect_param=config.KAFKA_CONNECT),
         'use': True,
+        'mode': 'single'
     },
-    'Clickhouse': {
+    'Clickhouse1': {
+        'storage': 'ClickHouse',
         'client': ClickhouseStorage(connect_param=config.CLICKHOUSE_CONNECT),
-        'use': True
+        'use': True,
+        'mode': 'single'
+    },
+    'Clickhouse2': {
+        'storage': 'ClickHouse',
+        'client': ClickhouseStorage(connect_param=config.CLICKHOUSE_CONNECT),
+        'use': True,
+        'mode': 'batch'
+    },
+    'DevNull': {
+        'storage': 'Dev/Null/',
+        'client': DevNullStorage(),
+        'use': True,
+        'mode': 'single'
     }
 }
