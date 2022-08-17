bfd456267f51fe5ffaec91a0200fc87449a834fa
Ilia Suvorkin <ilyafx@gmail.com>
2022-08-17 18:18:43 +0300

    add Postgree

--- research/bench/benchmarks.py
+++ research/bench/benchmarks.py
@@ -2,6 +2,7 @@ import config
 from hl_storage_kafka import KafkaStorage
 from hl_storage_clickhouse import ClickhouseStorage
 from hl_storage_null import DevNullStorage
+from hl_storage_pg import PostgresStorage
 
 BATCH_SEQUENCE = {
     'Kafka': {
@@ -10,15 +11,21 @@ BATCH_SEQUENCE = {
         'use': True,
         'mode': 'single'
     },
-    'Clickhouse1': {
+    'Clickhouse single': {
+        'storage': 'ClickHouse',
+        'client': ClickhouseStorage(connect_param=config.CLICKHOUSE_CONNECT),
+        'use': False,
+        'mode': 'single'
+    },
+    'Clickhouse batch': {
         'storage': 'ClickHouse',
         'client': ClickhouseStorage(connect_param=config.CLICKHOUSE_CONNECT),
         'use': True,
-        'mode': 'single'
+        'mode': 'batch'
     },
-    'Clickhouse2': {
-        'storage': 'ClickHouse',
-        'client': ClickhouseStorage(connect_param=config.CLICKHOUSE_CONNECT),
+    'Postgres batch': {
+        'storage': 'Postgres',
+        'client': PostgresStorage(connect_param=config.PG_DLS),
         'use': True,
         'mode': 'batch'
     },
