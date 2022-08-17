7864c04150716b4e1240b6421291ac2c9c02d0c4
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-17 19:21:30 +0300

    upgrade model

--- etl/src/models/event.py
+++ etl/src/models/event.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class Event(BaseModel):
     movie_id: str
     user_id: str
-    timestamp: int
+    event_time: datetime
+    view_run_time: int
