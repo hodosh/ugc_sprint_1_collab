8dd396e1a2fb84f0e993dac2e4f02cf7b4407692
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-18 17:58:31 +0300

    add data transformer

--- etl/src/services/clickhouse_loader.py
+++ etl/src/services/clickhouse_loader.py
@@ -1,29 +1,17 @@
-import typing as t
-
 from clickhouse_driver import Client
-
-from config import settings
 
 
 class ClickHouseLoader:
     def __init__(self,
                  host: str,
                  database: str,
-                 table: str,
                  batch_size: int = 500,
                  cluster: str = 'company_cluster'):
         self._host = host
         self._database = database
-        self._table = table
         self._batch_size = batch_size
         self._cluster = cluster
         self._client = None
-
-    @classmethod
-    def get_table_by_topic_name(cls, topic_name: str) -> str:
-        return {
-            settings.kafka_film_view_topic_name: settings.clickhouse_film_view_table,
-        }[topic_name]
 
     @property
     def client(self):
@@ -32,15 +20,18 @@ class ClickHouseLoader:
 
         return self._client
 
-    def load(self, data: t.Generator):
+    def load(self, data: dict):
         """
         Метод загружает данные в таблицу
         :param data: словарь данных
         """
-        for event_dict in data:
+        for table_name, data_list in data.items():
+            # create table if not exists
+            self.create_table(table_name)
+            # load data
             self.client.execute(
-                f'INSERT INTO {self._database}.{self._table} VALUES',
-                event_dict,
+                f'INSERT INTO {self._database}.{table_name} VALUES',
+                data_list,
             )
 
     def create_database(self):
@@ -49,11 +40,11 @@ class ClickHouseLoader:
         """
         self.client.execute(f'CREATE DATABASE IF NOT EXISTS {self._database} ON CLUSTER {self._cluster}')
 
-    def create_table(self):
+    def create_table(self, table_name: str):
         """
         Создаем таблицу, если она отсутствует
         """
-        self.client.execute(f'CREATE TABLE IF NOT EXISTS {self._database}.{self._table} ON CLUSTER {self._cluster} '
+        self.client.execute(f'CREATE TABLE IF NOT EXISTS {self._database}.{table_name} ON CLUSTER {self._cluster} '
                             '(id Int64, x Int32) Engine=MergeTree() ORDER BY id')
 
     def close(self):
