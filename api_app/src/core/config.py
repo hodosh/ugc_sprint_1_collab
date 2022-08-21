9628c1da174f1a6fb26e2a4c513fe353574b6737
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-21 20:07:30 +0300

    fix config & envs

--- api_app/src/core/config.py
+++ api_app/src/core/config.py
@@ -1,9 +1,8 @@
 import os
 from logging import config as logging_config
 
-from pydantic import BaseSettings, Field
-
 from core.logger import LOGGING
+from pydantic import BaseSettings, Field
 
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
@@ -11,14 +10,13 @@ logging_config.dictConfig(LOGGING)
 
 class Settings(BaseSettings):
     # Название проекта. Используется в Swagger-документации
-    PROJECT_NAME = Field(env='PROJECT_NAME', default='statistics')
+    PROJECT_NAME: str = Field(env='PROJECT_NAME', default='statistics')
 
     # Корень проекта
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    KAFKA_TOPIC = "views"
-    KAFKA_HOST = 'localhost'
-    KAFKA_PORT = 9092
+    KAFKA_FILM_VIEW_TOPIC: str = Field(env='KAFKA_FILM_VIEW_TOPIC', default='events_topic')
+    KAFKA_BROKERS: list = Field(env='KAFKA_BROKERS', default=['127.0.0.1:29092', '127.0.0.1:39092'])
 
 
 settings = Settings()
